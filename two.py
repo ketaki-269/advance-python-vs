4991a0739ba3973142e4aa576d293722ffcb5eb7
Ketaki Makode <ketakimakode@gmail.com>
2025-12-19 13:11:03 +0530

    Update file

--- two.py
+++ two.py
@@ -1,1 +1,9 @@
 #19-12-2025
+#19-12-2025
+import numpy as np 
+#list is heteronenious in nature ,mutable ,chnageable,comma separator ,enclosed in [] bracets
+# list of list 
+#array - dimensionality
+#1 dimensional 
+#two dimensional
+#multidimensional array
