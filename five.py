d4a91e2accfab22bd8430db832245acb6816e7ce
Ketaki Makode <ketakimakode@gmail.com>
2025-12-26 15:15:39 +0530

    Update file

--- five.py
+++ five.py
@@ -10,3 +10,9 @@
 # intration - we use  to perfrom interation on one dimentional array
 # one dimentional array
 
+#iteration
+#one dimentional
+import numpy as np
+a = np.array([1,2,3,4])
+for i in a :
+    print(i)
