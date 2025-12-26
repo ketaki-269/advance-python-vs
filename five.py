01fe3d5db8f81e2874eacc17accc7942a7d0673c
Ketaki Makode <ketakimakode@gmail.com>
2025-12-26 15:14:26 +0530

    Update file

---
+++ five.py
@@ -0,0 +1,12 @@
+#23-12-25
+#embedding- intration bana na
+#embedding or vector embedding important hai 
+
+
+#shapes me ineration kiya or shape me ek naya aya jo hai  puma,
+#ab puma brand bhi hai or animal bhi )> this condition is multiclass classifiction 
+# or ye sab packing or unpacking ke through discuss kerte hai interation ki help se 
+
+# intration - we use  to perfrom interation on one dimentional array
+# one dimentional array
+
