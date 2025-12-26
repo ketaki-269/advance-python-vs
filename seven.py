42b12bcc5ffda8c2ef2232dc93cd28ddd73da041
Ketaki Makode <ketakimakode@gmail.com>
2025-12-26 15:31:02 +0530

    Update file

---
+++ seven.py
@@ -0,0 +1,48 @@
+#26-12-25
+#revision or last classes
+# array or uske dimensions
+#vector embedding
+#binary classification 
+#where to use paratheses , sqaure bracekt,{},().
+
+# 4paarameter ------------------
+# with argu with return 
+# with  argu. with out retrun
+# with out argu with retrun  
+#with out rargument wuth our retrun 
+
+
+#0 dimensiona - one element ,one axis 
+#1 dimensiona - more than one  element ,one axis 
+# 2 dimensional - more  element ,two  axis 
+# 5 dimension - differnt element on differnt axis, 5 axis
+# differnce between divide and classification 
+
+
+# like ab ager Word PUMA aya toh ye matlab PUMA ek animal bhi hai or brand bhi toh vo kese decide kerega ki ye kis dimension me jainga toh 
+# is problem ko resolve kerne ke liye aya 
+# by act as brand data analyst so - 
+                                    # binary classification 
+
+# binarry - vector embedding - multiclass 
+# ndim - use to reshape dimension
+
+#indexing - 
+# with the hwlp of index we can chnage the value in array 
+# packing 
+# unpacking 
+
+#binary classification - example PUMA
+
+# binary ke badh hi multiclass pe ja sakte hai 
+#multiclass classification - example PUMA ke features 
+
+# fir ye sab ke badh 
+# aye data generation 
+# function se package , package ye library - library se module 
+
+# fir ye ke badh data frame banaya
+# or fir generated data save kiya c file me 
+# numpy se data generate kiya 
+#pandas se dataframe bana or usko read kerliya 
+# matplot or seaborn se  graphical representation then decision lenge 
