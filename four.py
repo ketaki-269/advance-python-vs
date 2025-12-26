23dac1b9ecc62c538e781ce8eecdb25e80d8761a
Ketaki Makode <ketakimakode@gmail.com>
2025-12-26 15:11:40 +0530

    Update file

--- four.py
+++ four.py
@@ -4,3 +4,13 @@
 a= [1,2,3,4,5,6]
 for i in a :
     print(i)
+
+
+
+# no of row - i 
+# i jis jagah thak jana hai  
+# j us e to assign value 
+# no of col - j
+n = 5
+for i in range(1, n + 1):
+    print(" " * (n - i) + "*" * (2*i - 1))    
