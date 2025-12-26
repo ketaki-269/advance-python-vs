6ee1392adb586af2b750ca7395a4a74c177cd5e5
Ketaki Makode <ketakimakode@gmail.com>
2025-12-26 15:17:41 +0530

    Update file

--- five.py
+++ five.py
@@ -16,3 +16,18 @@ import numpy as np
 a = np.array([1,2,3,4])
 for i in a :
     print(i)
+
+
+#two dimentional array(iteration)
+arr2 = np.array([[1,2,3,4] , [5,6,7,8]])
+for i in arr2:
+    for j in i:
+        print(j)
+
+#three dimentional array
+arr3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr2)
+for i in arr3:
+    for j in i:    
+        for k in j:
+            print(k)        
