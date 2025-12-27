3374b9125b36ea184429b788045e7190188648ee
Ketaki Makode <ketakimakode@gmail.com>
2025-12-27 17:51:08 +0530

    Update file

--- eight.py
+++ eight.py
@@ -1,3 +1,10 @@
 #27-12-2025
 # EDA- YE JO APNA LAST ME PADHENGE 
 # EDA - EXPLORATORY DATA ANALYSIS
+
+import numpy as np # use for numerical operation 
+import pandas as pd # use for data preprocessing ,data transformation, data manipulation 
+import matplotlib.pyplot as plt # use for standard and basic repersentation
+import seaborn as sns # use for advance graphical representation
+
+# when we wotk on all these lib combine  we do EDA,then we make report , 
